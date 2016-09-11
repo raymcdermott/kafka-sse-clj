36571c511fd9e6c536feb229093c602385c59c9c
Ray McDermott <ray@mcdermott.be>
2016-09-11 21:11:03 +0200

    improved the locality of the logic and cleaned up the config code

--- examples/heroku/heroku_kafka.clj
+++ examples/heroku/heroku_kafka.clj
@@ -8,19 +8,11 @@
            (com.github.jkutner EnvKeyStore)
            (org.apache.kafka.common.config SslConfigs)))
 
-(defn env-or-fail [env-var-name]
+(defn- env-or-fail
+  [env-var-name]
   (if-let [env-var (env env-var-name)]
     env-var
-    (throw (Exception. (str "Must set environment variable: " env-var-name)))))
-
-(def broker-url (env-or-fail :kafka-url))
-(def _ (env-or-fail :kafka-trusted-cert))
-(def _ (env-or-fail :kafka-client-cert))
-(def _ (env-or-fail :kafka-client-cert-key))
-
-; This is only an option for Heroku Private Spaces (HPS)
-(def hps-broker-url (env :kafka-plaintext-url))
-
+    (throw (RuntimeException (str "Must set environment variable: " env-var-name)))))
 
 (defn- as-properties
   [m]
@@ -28,18 +20,18 @@
     (doseq [[n v] m] (.setProperty props n v))
     props))
 
-
 (defn kafka-connection-config
   "Return a Java Properties object that contains the broker configuration"
   []
-  (if hps-broker-url
+  (if-let [hps-broker-url (env :kafka-plaintext-url)]
     (as-properties {CommonClientConfigs/BOOTSTRAP_SERVERS_CONFIG hps-broker-url})
     ; if not in Heroku Private Space, brokers are always accessed using SSL
-    (let [brokers (map (fn [url]
-                         (let [u (URI. url)]
-                           (str (.getHost u) ":" (.getPort u)))) (str/split broker-url #","))
+    (let [_ (env-or-fail :kafka-trusted-cert)
+          _ (env-or-fail :kafka-client-cert)
+          _ (env-or-fail :kafka-client-cert-key)
+          broker-url (env-or-fail :kafka-url)
+          brokers (map  #(str (.getHost (URI. %)) ":" (.getPort (URI. %))) (str/split broker-url #","))
           broker-config {CommonClientConfigs/BOOTSTRAP_SERVERS_CONFIG (str/join "," brokers)}
-          security-config {CommonClientConfigs/SECURITY_PROTOCOL_CONFIG "SSL"}
           env-trust-store (EnvKeyStore/createWithRandomPassword "KAFKA_TRUSTED_CERT")
           env-key-store (EnvKeyStore/createWithRandomPassword "KAFKA_CLIENT_CERT_KEY" "KAFKA_CLIENT_CERT")
           trust-store (.storeTemp env-trust-store)
@@ -49,6 +41,7 @@
                       SslConfigs/SSL_TRUSTSTORE_PASSWORD_CONFIG (.password env-trust-store)
                       SslConfigs/SSL_KEYSTORE_TYPE_CONFIG       (.type env-key-store)
                       SslConfigs/SSL_KEYSTORE_LOCATION_CONFIG   (.getAbsolutePath key-store)
-                      SslConfigs/SSL_KEYSTORE_PASSWORD_CONFIG   (.password env-key-store)}]
-      (as-properties (merge broker-config security-config ssl-config)))))
+                      SslConfigs/SSL_KEYSTORE_PASSWORD_CONFIG   (.password env-key-store)}
+          security-config {CommonClientConfigs/SECURITY_PROTOCOL_CONFIG "SSL"}]
+      (as-properties (merge broker-config ssl-config security-config)))))
 
